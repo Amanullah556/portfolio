86097c5afa136f9502516bdc85f2d8583b168862
Aman ullah <amankhanbangash39@gmail.com>
2026-06-23 15:33:37 -0700

    Updated portfolio image

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -19,7 +19,7 @@ export default function Hero() {
 
                 <div className="rounded-full bg-gradient-to-r from-cyan-400 via-purple-500 to-cyan-400 p-[3px] shadow-[0_0_40px_rgba(34,211,238,0.45)]">
                     <img
-                        src="./src/assets/profile.png"
+                        src="/profile.png"
                         className="h-32 w-32 rounded-full border-4 border-[#05050a] md:h-40 md:w-40"
                     />
                 </div>
